6ff04fe6c6acbce401e71c996791d54bb6e8c137
tarodo <rsvolozhanin@gmail.com>
2023-05-23 11:38:53 +0400

    compare pydub and ffmpeg im memory usage

--- tg/main.py
+++ tg/main.py
@@ -4,6 +4,7 @@ import os, psutil
 from telegram import Update
 from telegram.ext import Application, ContextTypes, MessageHandler, filters
 from yt_dlp import YoutubeDL
+import ffmpeg
 
 from pydub import AudioSegment
 from pathlib import Path
@@ -58,10 +59,47 @@ def show_memory(txt: str = "") -> None:
     logger.info(f"Memory : {memory_size} Mb : {txt}")
 
 
+def get_audio_duration(file_path):
+    probe = ffmpeg.probe(file_path)
+    audio_duration = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
+    return float(audio_duration['duration'])
+
+
+def split_ffmpeg_file(file_path: Path) -> int:
+    show_memory("Start ffmpeg splitting")
+    segment_duration = FILE_LENGTH * 60
+    total_duration = get_audio_duration(file_path)
+    show_memory("After duration count")
+    start_times = [start for start in range(0, int(total_duration), segment_duration)]
+    show_memory("After count start times")
+    for i, start_time in enumerate(start_times):
+        show_memory(f"{i+1} iteration")
+        new_file_name = f"{file_path.stem}_p{i+1}.mp3"
+        new_file_path = file_path.with_name(new_file_name)
+        (
+            ffmpeg
+            .input(file_path, ss=start_time, t=segment_duration)
+            .output(str(new_file_path))
+            .run(cmd=['ffmpeg', '-loglevel', 'error'])
+        )
+    return len(start_times)
+
+
+def split_files_ffmpeg():
+    for filename in get_new_files():
+        file_path = Path(f"{FOLDER_PATH}/{filename}")
+        if not file_path.suffix == ".mp3":
+            continue
+        audio_size = round(os.path.getsize(file_path) / (1024 ** 2), 2)
+        logger.info(f"Audio size :: {audio_size} Mb")
+        parts_cnt = split_ffmpeg_file(file_path)
+        if parts_cnt:
+            os.remove(file_path)
+
+
 def split_mp3_file(file_path: Path) -> int:
     logger.info(f"Start file splitting :: {file_path}")
-    audio_size = round(os.path.getsize(file_path) / (1024 ** 2), 2)
-    logger.info(f"Audio size :: {audio_size} Mb")
+
     show_memory("Start split")
 
     audio = AudioSegment.from_file(file_path)
@@ -87,6 +125,8 @@ def split_files():
         file_path = Path(f"{FOLDER_PATH}/{filename}")
         if not file_path.suffix == ".mp3":
             continue
+        audio_size = round(os.path.getsize(file_path) / (1024 ** 2), 2)
+        logger.info(f"Audio size :: {audio_size} Mb")
         parts_cnt = split_mp3_file(file_path)
         if parts_cnt:
             os.remove(file_path)
@@ -98,6 +138,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("It is not youtube link")
         return
     download_yt(url)
+    split_files_ffmpeg()
     split_files()
 
     for filename in get_new_files():
