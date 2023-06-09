8a6d6020363d782da457318773a8c53802664dd0
tarodo <rsvolozhanin@gmail.com>
2023-06-09 07:32:19 +0400

    add lifecycle rules for files deleting

--- tg/load_s3.py
+++ tg/load_s3.py
@@ -2,6 +2,9 @@ import logging
 from datetime import timedelta
 from pathlib import Path
 
+from minio.commonconfig import ENABLED, Filter
+from minio.lifecycleconfig import Expiration, LifecycleConfig, Rule
+
 from minio import Minio
 
 logger = logging.getLogger(__name__)
@@ -11,12 +14,26 @@ def get_minio_client(url: str, port: str, user: str, secret: str) -> Minio:
     return Minio(f"{url}:{port}", access_key=user, secret_key=secret, secure=False)
 
 
+def get_lifecycle():
+    return LifecycleConfig(
+        [
+            Rule(
+                rule_id="Expire in 48 hours",
+                rule_filter=Filter(prefix=""),
+                status=ENABLED,
+                expiration=Expiration(days=2),
+            )
+        ]
+    )
+
+
 def upload_file_to_minio(client: Minio, file_path: Path, bucket_name: str):
     file_name = file_path.name
     logger.info(f"Start send file : {file_name} :: into : {bucket_name}")
 
     if not client.bucket_exists(bucket_name):
         client.make_bucket(bucket_name)
+        client.set_bucket_lifecycle(bucket_name, get_lifecycle())
     try:
         client.fput_object(bucket_name, file_name, file_path)
 
